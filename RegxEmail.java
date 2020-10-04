ea971f4aaf4b4998b83ca012da72b0e0d11fdedc
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-04 20:39:22 +0530

    Validating @ part

--- RegxEmail.java
+++ RegxEmail.java
@@ -1,5 +1,18 @@
 package regx;
 
-public class RegxEmail {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class RegxEmail 
+{
+	public static void main(String[] args)
+	{
+	    Scanner c=new Scanner(System.in);
+	    System.out.println("Enter pincode");
+	    String n=c.nextLine();
+	    System.out.println("String is"+n);
+		System.out.println(Pattern.matches("^[_A-Za-z0-9-\\+]+@"
+				+"([A-Za-z]{2,})$",n));
+		c.close();
+	}   
 }
